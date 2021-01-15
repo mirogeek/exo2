152447521565f03b27735c005ce897601553a873
amir rebbah <amir.rebbah16@gmail.com>
2021-01-15 13:23:59 +0100

    part2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import car from"./car.jpg"
+import popo from"./popo.jpg"
+
 
 
 function App() {
   return (
     <div className="App">
-      
+      <img src={car}  alt="logo" />
+      <img src={popo} alt="logo2" />
+
     </div>
   );
 }
